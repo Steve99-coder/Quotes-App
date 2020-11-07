05586b650be386fb8d24a03536ed509bc39dbd36
Steve99-coder <stevenderitu99@gmail.com>
2020-11-07 16:07:51 +0300

    Add the class quote

---
+++ src/app/quote.ts
@@ -0,0 +1,19 @@
+export class Quote {
+    id:number;
+    author:string;
+    state:string;
+    user:string;
+    day:Date;
+    upVotes:number;
+    downVotes:number;
+
+    constructor(id:number, state:string, author:string, user:string, day:Date){
+        this.id=id;
+        this.author=author;
+        this.state=state; 
+        this.user= user;
+        this.day=day;
+        this.upVotes=0;
+        this.downVotes=0;
+    }
+}
