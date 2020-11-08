f059e5ad4cc245adda152d086e45ab2333093dc5
Steve99-coder <stevenderitu99@gmail.com>
2020-11-08 11:41:14 +0300

    Edit the folder

--- src/app/favhighlight.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FavhighlightDirective } from './favhighlight.directive';
-
-describe('FavhighlightDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FavhighlightDirective();
-    expect(directive).toBeTruthy();
-  });
-});
